ce29c60bb13afcab67b13b7d7925604e77d79d5a
syarifa20 <syaripah2004@gmail.com>
2025-01-29 13:58:22 +0700

    jwt auth laravel

---
+++ app/Http/Controllers/YourController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class YourController extends Controller
+{
+    public function index(){
+        return 'berhasil masuk';
+    }
+}
